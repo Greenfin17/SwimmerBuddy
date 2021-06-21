5894e41d4e8e8488fa8e304134a87a15c8c99e06
Greenfin17 <jmaple@yahoo.com>
2021-06-21 18:30:38 -0500

    adjusting title on shared workouts page

--- src/views/SharedWorkoutsView.js
+++ src/views/SharedWorkoutsView.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'reactstrap';
-import SharedWorkoutCard from '../components/cards/SharedWorkoutCard';
+import WorkoutCard from '../components/cards/WorkoutCard';
 import TitleBox from '../components/TitleBox';
 import { getPublicWorkouts } from '../helpers/data/workoutData';
 
@@ -34,7 +34,7 @@ const SharedWorkoutsView = ({
 
   return (
   <div className='workouts-view'>
-    <TitleBox heading1='Workouts' />
+    <TitleBox heading1='Shared Workouts' />
     { user
       && <div className='view-button-container'>
         <Button className='btn btn-info add-workout'
@@ -43,7 +43,7 @@ const SharedWorkoutsView = ({
     }
     <div className='card-container workout-cards-container'>
       { publicWorkouts.map((workout) => user?.uid !== workout.author_uid
-        && <SharedWorkoutCard
+        && <WorkoutCard
         key={workout.id}
         workout={workout}
         user={user}
